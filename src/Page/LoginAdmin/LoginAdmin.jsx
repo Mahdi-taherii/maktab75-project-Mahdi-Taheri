f680f458dee269b6b456de5ebda0c45b311a6394
M-Taheri <tmhdy166@gmail.com>
2022-08-19 09:07:45 +0430

    build(deps): update package-lock.json, package.json, Login.module.css and LoginAdmin.jsx

--- src/Page/LoginAdmin/LoginAdmin.jsx
+++ src/Page/LoginAdmin/LoginAdmin.jsx
@@ -1,31 +1,37 @@
 import axios from "axios";
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "./Login.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const LoginAdmin = () => {
-  const [emailValue , setEmailValue] = useState('');
-  const [passwordValue , setPasswordValue] = useState();
-  const [login , setLogin] = useState();
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState();
+  const [login, setLogin] = useState();
+  let navigate = useNavigate();
+  const notify = () => toast.error("رمز اشتاه است ");
+  const totify = () => toast.success("خوش امدید ")
 
-  const Data ={
+  const Data = {
     username: emailValue,
-    password : passwordValue
-  }
+    password: passwordValue,
+  };
 
- const onLogInClicked =()=>{
-      axios({
-      method: 'post',
-      url: 'http://localhost:3003/auth/login',
-      headers: { 
-        'Content-Type': 'application/json'
-      },
-      data : Data
-    })
-      .then((response)=>{
-        setLogin(response.data)
+  const onLogInClicked =async () => {
+    const response = await axios.post("http://localhost:3003/auth/login",Data)
+      .then((res) => {
+       localStorage.setItem("TOKEN", JSON.stringify(res.data.token));
+       setTimeout(() => {
+        navigate("/Management");
+       }, 1000);
+       totify()
       })
- }
+      .catch((err) => {
+        notify();
+        localStorage.clear()
+      });
+  };
 
   return (
     <Fragment>
@@ -38,32 +44,44 @@ export const LoginAdmin = () => {
                 type="text"
                 placeholder="نام کاربری"
                 value={emailValue}
-                onChange={(e)=>setEmailValue(e.target.value)}
+                onChange={(e) => setEmailValue(e.target.value)}
                 required=""
                 id={styled.username}
               />
-              {!emailValue ? <p className="text-danger">لطفا فیلد مورد نظر را پر کنید</p> : <p></p>}
+              {!emailValue ? (
+                <p className="text-danger">لطفا فیلد مورد نظر را پر کنید</p>
+              ) : (
+                <p></p>
+              )}
             </div>
             <div>
               <input
                 type="password"
                 placeholder="رمز عبور"
                 value={passwordValue}
-                onChange={(e)=>setPasswordValue(e.target.value)}
+                onChange={(e) => setPasswordValue(e.target.value)}
                 required=""
                 id={styled.password}
               />
-              {!passwordValue ? <p className="text-danger">لطفا فیلد مورد نظر را پر کنید</p> : <p></p>}
+              {!passwordValue ? (
+                <p className="text-danger">لطفا فیلد مورد نظر را پر کنید</p>
+              ) : (
+                <p></p>
+              )}
             </div>
             <div>
-              <Link to="/Management"><input type="submit" onClick={onLogInClicked} disabled={!emailValue || !passwordValue} value="ورود" /> </Link>
+              <input
+                type="button"
+                onClick={onLogInClicked}
+                disabled={!emailValue || !passwordValue}
+                value="ورود"
+              />
             </div>
           </form>
-          <div className={styled.button}>
-          </div>
+          <div className={styled.button}></div>
         </section>
+        <ToastContainer />
       </div>
     </Fragment>
   );
-}
-
+};
